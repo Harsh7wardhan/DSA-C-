017081d574902391b7b4df05c1c5856ada443aed
Harshwardhan Natu <harsh07natu@gmail.com>
2022-05-13 10:20:38 +0530

    1299 Leetcode

--- replacewgreatestelle.cpp
+++ replacewgreatestelle.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int mx = -1;
+        int max_num = -1;
         vector<int> res = arr;
-
-        for(int i=arr.size()-1;i>=0;i--)
-        {
-            res[i]=max;
-            mx=max(max,arr[i]);
+        for (int i=arr.size()-1; i>=0; i--) {
+            res[i] = max_num;
+            max_num = max(max_num, arr[i]);
         }
-
         return res;
     }
 };
