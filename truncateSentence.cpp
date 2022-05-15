79f4f4d96357ff3734130a5cc83b61aeaa6471b9
Harshwardhan Natu <harsh07natu@gmail.com>
2022-05-15 14:34:20 +0530

    Add files via upload

---
+++ truncateSentence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) 
+    {
+        int count= 0;
+        string ans="";
+
+        for(auto x : s)
+        {
+
+            if(x==" ")
+                count++;
+            
+            if(count==k)
+                break;
+
+            ans+=x;
+        }
+        return ans;
+    }
+};
+
+//
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int count =0;
+        string ans="";
+        for(auto x:s)
+        {
+            if(x==' ')
+                count++;
+            if(count==k)
+                break;
+            ans+=x;
+        }
+        
+        return ans;
+    }
+};
