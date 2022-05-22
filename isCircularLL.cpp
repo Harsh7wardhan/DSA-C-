936bb9eb258af49eac6ef23fe1c74e5410ed1ccd
Harshwardhan Natu <harsh07natu@gmail.com>
2022-05-22 17:26:18 +0530

    LL

---
+++ isCircularLL.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+	
+		// if head is NULL then return false;
+        if(head == NULL)
+            return false;
+        
+		// making two pointers fast and slow and assignning them to head
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+		// till fast and fast-> next not reaches NULL
+		// we will increment fast by 2 step and slow by 1 step
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+			
+			// At the point if fast and slow are at same address
+			// this means linked list has a cycle in it.
+            if(fast == slow)
+                return true;
+        }
+        
+		// if traversal reaches to NULL this means no cycle.
+        return false;
+    }
+};
+
+//approach 2
+
+bool isCircular(Node* head)
+{
+  if(head==NULL)
+  {
+    return true;
+    
+}
+unordered_map<Node* ,bool> m;
+  m[head]=true;
+Node* temp=head->next;
+  
+ while(temp!=NULL && temp!=head)
+    { 
+      m[temp]=true;
+      temp=temp->next;
+}
+
+  if(m[temp])
+  {
+    return true;
+
+    }
+  else{
+    return false;
+}
+  
+}
