0b582fc689b8693342d48d73802b2397fd5e3e0a
Harshwardhan Natu <harsh07natu@gmail.com>
2022-07-21 20:29:14 +0530

    Add files via upload

---
+++ DLLreverse.cpp
@@ -0,0 +1,15 @@
+//doubly linked list reverse
+Node* reverseDLL(Node * head)
+{
+   Node* curr=head;
+   Node* pre=NULL;
+   Node* forward=NULL;
+   while(curr!=NULL){
+       forward=curr->next;
+       curr->prev=curr->next;
+       curr->next=pre;
+       pre=curr;
+       curr=forward;
+   }
+   return pre;
+}
