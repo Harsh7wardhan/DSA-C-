b45e0f8e6bc2200162391bf187b5f9c4e7118372
Harshwardhan Natu <harsh07natu@gmail.com>
2022-07-27 21:23:33 +0530

    Add files via upload

---
+++ twosum2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int low = 0;
+        int high = numbers.size()-1;
+        vector<int> sum;
+        
+        while(low<=high)
+        {
+            if(numbers[low]+numbers[high]==target)
+            {
+                sum.push_back(low+1);
+                sum.push_back(high+1);
+                return sum;
+            }
+            else if(numbers[low]+numbers[high]>target)
+            {
+                high--;
+            }
+            else
+            {
+                low++;
+            }
+        }
+        return sum;
+    }
+};
