f24cf42be3dc078004597478c52b940641655112
Harshwardhan Natu <harsh07natu@gmail.com>
2022-08-29 22:32:14 +0530

    Add files via upload

--- grid unique paths.cpp
+++ grid unique paths.cpp
@@ -1,18 +1,79 @@
+// MEMOIZATION 1
+class Solution
+{
+public:
+    int solve(int i, int j, int m, int n, vector<vector<int>> &dp)
+    {
+        if (i >= m || j >= n)
+            return 0;
+        if (i == m - 1 && j == n - 1)
+            return 1;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        return dp[i][j] = solve(i + 1, j, m, n, dp) + solve(i, j + 1, m, n, dp);
+    }
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(0, 0, m, n, dp);
+    }
+};
+
+// MEMOIZATION 2
+class Solution
+{
+public:
+    int f(int i, int j, vector<vector<int>> &dp)
+    {
+        if (i == 0 && j == 0)
+            return 1;
+        if (i < 0 || j < 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int up = f(i - 1, j, dp);
+        int left = f(i, j - 1, dp);
+
+        return dp[i][j] = up + left;
+    }
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(m - 1, n - 1, dp);
+    }
+};
+
+//BOTTOM UP
 class Solution {
 public:
-    int solve(int i,int j,int m,int n,vector<vector<int>> &dp)
-    {
-        if(i>=m||j>=n)
-            return 0;
-        if(i==m-1&&j==n-1)
-            return 1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        return dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
+    int f(int m,int n,vector<vector<int>> &dp){
+       for(int i=0; i<m ;i++){
+      for(int j=0; j<n; j++){
+          
+          //base condition
+          if(i==0 && j==0){
+              dp[i][j]=1;
+              continue;
+          }
+          
+          int up=0;
+          int left = 0;
+          
+          if(i>0) 
+            up = dp[i-1][j];
+          if(j>0)
+            left = dp[i][j-1];
+            
+          dp[i][j] = up+left;
+      }
+  }
+  
+  return dp[m-1][n-1];
     }
     int uniquePaths(int m, int n) {
-      vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(0,0,m,n,dp);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(m,n,dp);
     }
 };
