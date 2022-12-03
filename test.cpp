04d5879d93ae1f400278a95da17f5b980f66c9f7
Harshwardhan Natu <harsh07natu@gmail.com>
2022-12-04 00:13:26 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[6]={1,2,3,4,5,6};
+
+    for(int i=0;i<6;i+=2)
+    {
+        if(i+1<6)
+        {
+            swap(arr[i],arr[i+1]);
+        }
+    }
+
+    for(int i=0;i<6;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
