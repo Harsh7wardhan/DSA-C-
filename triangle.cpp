56b8676bd878a67d0ae1b9088847237fc77ec01b
Harshwardhan Natu <harsh07natu@gmail.com>
2023-03-19 20:28:59 +0530

    Add files via upload

--- triangle.cpp
+++ triangle.cpp
@@ -1,19 +1,54 @@
 //recursion
 class Solution {
 public:
-    int helper(int i,int j,vector<vector<int>>& triangle){
-        int m = triangle.size();
-        int n = triangle[0].size();
-        if(i==m-1) return triangle[m-1][j];
-        int down = triangle[i][j] + helper(i+1,j,triangle);
-        int diag = triangle[i][j] + helper(i+1,j+1,triangle);
-        
-        return min(down,diag);
-    }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
-        int n = triangle[0].size();
-        
-        return helper(0,0,triangle);
-    }
+	int solve(int i, int j, int n, vector<vector<int>>&triangle) {
+		if(i == n - 1)
+			return triangle[i][j];
+		int bottom = triangle[i][j] + solve(i+1, j, n, triangle);
+		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle);
+		return min(bottom, bottom_right);
+	}
+	int minimumTotal(vector<vector<int>>& triangle) {
+		int n = triangle.size();
+		return solve(0, 0, n, triangle);
+	}
+};
+
+//memoization
+class Solution {
+public:
+	int solve(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp) {
+		if(i == n - 1)
+			return triangle[i][j];
+		if(dp[i][j] != -1)
+			return dp[i][j];
+		int bottom = triangle[i][j] + solve(i+1, j, n, triangle, dp);
+		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle, dp);
+		return dp[i][j] = min(bottom, bottom_right);
+	}
+	int minimumTotal(vector<vector<int>>& triangle) {
+		int n = triangle.size();
+		vector<vector<int>>dp(n, vector<int>(n, -1));
+		return solve(0, 0, n, triangle, dp);
+	}
+};
+
+//tabulation
+class Solution {
+public:
+	 // Tabulation
+	int minimumTotal(vector<vector<int>>& triangle) {
+		int n = triangle.size();
+		vector<vector<int>>dp(n, vector<int>(n, -1));
+		for(int i = 0; i < n; i++) 
+			dp[n-1][i] = triangle[n-1][i];
+		for(int i = n-2; i >= 0; i--) {
+			for(int j = i; j >= 0; j--) {
+				int up = triangle[i][j] + dp[i+1][j];
+				int up_left = triangle[i][j] + dp[i+1][j+1];
+				dp[i][j] = min(up, up_left);
+			}
+		}
+		return dp[0][0];
+	}
 };
