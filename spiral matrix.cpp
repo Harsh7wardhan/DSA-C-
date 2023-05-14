7007dbab8e9b05ade7cbc901557213afae9a25bf
Harshwardhan Natu <harsh07natu@gmail.com>
2023-05-14 21:52:18 +0530

    Add files via upload

--- spiral matrix.cpp
+++ spiral matrix.cpp
@@ -1,47 +1,75 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
 class Solution
-{
-public:
-    vector<int> spiralOrder(vector<vector<int>> &matrix)
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
     {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int left = 0, right = m - 1, bottom = n - 1, top = 0;
-        int direction = 1;
-        vector<int> v;
-        while (left <= right && top <= bottom)
-        {
-            if (direction == 1)
-            {
-                for (int i = left; i <= right; i++)
-                    v.push_back(matrix[top][i]);
-                direction = 2;
-                top++;
+        // code here 
+        int left = 0 , right = c-1 , top = 0, bottom = r-1;
+        vector<int> ans;
+        while(left<=right && top<=bottom){
+            //right
+            for(int i=left;i<=right;i++){
+                ans.push_back(matrix[top][i]);
             }
-
-            else if (direction == 2)
-            {
-                for (int i = top; i <= bottom; i++)
-                    v.push_back(matrix[i][right]);
-                direction = 3;
-                right--;
+            top++;
+            
+            for(int i=top;i<=bottom;i++){
+                ans.push_back(matrix[i][right]);
             }
-
-            else if (direction == 3)
-            {
-                for (int i = right; i >= left; i--)
-                    v.push_back(matrix[bottom][i]);
-                direction = 4;
+            right--;
+            
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                ans.push_back(matrix[bottom][i]);
+            }
                 bottom--;
-            }
-
-            else if (direction == 4)
-            {
-                for (int i = bottom; i >= top; i--)
-                    v.push_back(matrix[i][left]);
-                direction = 1;
+         }       
+            
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    ans.push_back(matrix[i][left]);
+                }
                 left++;
             }
+            
         }
-        return v;
+         return ans;   
     }
+    
 };
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int r,c;
+        cin>>r>>c;
+        vector<vector<int> > matrix(r); 
+
+        for(int i=0; i<r; i++)
+        {
+            matrix[i].assign(c, 0);
+            for( int j=0; j<c; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code EndsF
